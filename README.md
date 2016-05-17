9ada49a0e10e308743615ae153353d14e2c087a7
NotGeekAtAll <254489973@qq.com>
2016-05-17 11:42:51 +0800

    dropsearch

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # DropSearch
-模糊搜索下拉选择插件
+模糊搜索下拉选择插件（pc端，依赖jquery）
+
+基本效果如下（实现了鼠标选择，键盘上下和回车键选择）:
+![DropSearch](/dropsearch.gif)
+
+##如何使用
+
+    <link rel="stylesheet" type="text/css" href="dropsearch.css">
+    <script src="http://cdnjs.w3cbus.com/jquery/2.0.0/jquery.min.js"></script>
+	<script src="dropsearch.js"></script>
+
+##注意
+
+ - css样式可根据自己情况设置
+ - 渲染列表中的list，请根据后台给的接口自己调整，具体在dropsearch.js中的getNameList()函数中ajax的succes回调中（这里有点坑QAQ）
+
+
+
+##快速上手
+
+    DropSearch.init({
+				selector:"seletor",
+				url: URL,
+				inputParam:"name"
+			});
+			
+
+
+##参数说明
+
+###selector [string]
+input的选择器（单个）
+
+
+###url [string]
+请求的url地址
+
+###inputParam [string]
+请求的url地址中input的值为值，inputParam为键（如?inputParam=input）
+
+###otherParams(可选) [obj]
+请求的url地址中其他参数，如：{otherParams1:otherParamsValue1,otherParams2:otherParamsValue2}
+
+###clickHandler(回调，可选) [function(data)]
+点击或回车下拉选项时的回调函数，参数data为点击项的值
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dropsearch.js
@@ -0,0 +1,112 @@
+(function($){
+	
+	var DropSearch = function (options) {
+		var self = this;
+		this.inputField = $(options.selector);
+		this.searchUrl = options.url;
+		this.inputParam = options.inputParam;
+		this.otherParams = options.otherParams;
+		this.oUl = $("<ul class='drop-list'></ul>");
+		this.index = -1;
+		this.options = options;
+		
+		this.inputField.on("keyup click",function(e){
+			var e = e || window.event;
+			e.stopPropagation();
+			
+			if ( e.type == "click") {
+				self.renderList(self.getNameList());
+			} else{
+				if ( e.keyCode == 40 && self.oUl.children().length > 0) {
+					self.doMove(1);
+				}else if ( e.keyCode == 38 && self.oUl.children().length > 0){
+					self.doMove(-1);
+				}else if ( e.keyCode == 13 && self.oUl.children().length > 0 && self.oUl.children().hasClass("active")){
+					self.doInput(self.oUl.children().filter("[class='active']").html());
+				}else {
+					self.renderList(self.getNameList());
+				}
+			}
+			
+		});
+		
+		
+		this.oUl.on("click",function(e){
+			var e = e || window.event;
+			e.stopPropagation();
+			if ( e.target.nodeName === "LI") {
+				self.doInput(e.target.innerHTML)
+			}
+		});
+		
+		$(document).on("click",function(){
+			self.oUl.empty();
+		});
+	}
+	
+	DropSearch.prototype.getNameList = function () {
+		var self = this,
+			list = [],
+			param = [];
+		param.push(this.inputParam + "=" + this.inputField.val());
+		if ( this.otherParams) {
+			for (var item in this.otherParams) {
+				param.push( item + "=" + this.otherParams[item]);
+			}
+		}
+		$.ajax({
+			type: "post",
+			url: self.searchUrl,
+			data: param.join("&"),
+			async: false,
+			success: function (data){
+				$.each(data.data, function(i,item) {
+					list.push(item.name);
+				});
+			}
+		});
+		return list;
+	}
+ 	
+ 	DropSearch.prototype.renderList = function (list) {
+ 		this.index = -1;
+ 		var self = this,
+ 			html = [];
+ 		self.oUl.empty();
+ 		$.each(list, function(i) {
+ 			html += "<li>"+list[i]+"</li>";
+ 		});
+ 		self.oUl.append(html);
+ 		self.inputField.parent().append(self.oUl).css("position","relative");
+ 		self.oUl.css({
+ 			top: self.inputField.outerHeight()+2,
+ 			left: self.inputField.get(0).offsetLeft,
+ 			width: self.inputField.outerWidth()
+ 		});
+ 	}
+ 	
+ 	DropSearch.prototype.doMove = function (count) {
+ 		var aLi = this.oUl.children();
+ 		this.index = this.index + count;
+ 		if ( this.index > aLi.length - 1) {
+ 			this.index = 0;
+ 		} else if ( this.index < 0){
+ 			this.index = aLi.length -1;
+ 		}
+ 		aLi.removeClass("active").eq(this.index).addClass("active");
+ 	}
+ 	
+ 	DropSearch.prototype.doInput = function (data){
+ 		this.inputField.val(data);
+		this.oUl.empty();
+		if ( this.options.clickHandler) {
+			this.options.clickHandler(data);
+		}
+ 	}
+ 	
+	DropSearch.init = function(options){
+		new this(options);
+	};
+	
+	window["DropSearch"] = DropSearch;
+})(jQuery);
