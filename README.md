57daccf3cd1accb679c98ce837a0d39d7861e3de
notgeekatall <254489973@qq.com>
2017-07-07 14:00:08 +0800

    优化

--- README.md
+++ README.md
@@ -4,49 +4,57 @@
 基本效果如下（实现了鼠标选择，键盘上下和回车键选择）:
 ![DropSearch](/dropsearch.gif)
 
-##如何使用
+## 如何使用
 
     <link rel="stylesheet" type="text/css" href="dropsearch.css">
-    <script src="http://cdnjs.w3cbus.com/jquery/2.0.0/jquery.min.js"></script>
-	<script src="dropsearch.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script src="dropsearch.js"></script>
 
-##注意
+## 注意
 
  - css样式可根据自己情况设置
- - 渲染列表中的list，请根据后台给的接口自己调整，具体在dropsearch.js中的getNameList()函数中ajax的succes回调中（这里有点坑QAQ）
 
 
-
-##快速上手
+## 快速上手
 
     DropSearch.init({
-				selector:"#id",
-				url: "URL",
-				inputParam:"name"
+		selector:"#id",
+		url: "URL",
+		inputParam:"name",
+		ajaxHandler: function(data, list) {
+			$.each(data.data, function(i, item) {
+				list.push(item.name);
 			});
+		}
+	});
 			
 
 
-##配置项说明
+## 配置项说明
 
-###selector [string]
-input的选择器（单个）
+### selector(必填) [String]
+input的选择器,
 
-
-###url [string]
+### url(必填) [string]
 请求的url地址
 
-###ajaxType(可选) [string]
+### debounceTime(可选) [Number]
+输入时防抖的消除setTimeout的时间，默认为300ms
+
+### ajaxType(可选) [String]
 请求类型,默认为post
 
-###inputParam [string]
+### inputParam(必填) [String]
 请求的url地址中input的值为值，inputParam为键（如?inputParam=input）
 
-###otherParams(可选) [obj]
+### otherParams(可选) [Object]
 请求的url地址中其他参数，如：{otherParams1:otherParamsValue1,otherParams2:otherParamsValue2}
 
-###clickHandler(回调，可选) [function(data)]
+### clickHandler(回调，可选) [function(data)]
 点击或回车下拉选项时的回调函数，参数data为点击项的值
+
+### ajaxHandler(回调，必填) [function(data,list)]
+ajax请求接口成功的回调函数，data为请求的结果。list为下拉框填充的数据，因为接口返回的值得结构不一样，所以根据自己的情况来填充list(具体看示例)。
 
 
 
--- dropsearch.js
+++ dropsearch.js
@@ -1,113 +1,132 @@
-(function($){
-	
-	var DropSearch = function (options) {
-		var self = this;
-		this.inputField = $(options.selector);
-		this.searchUrl = options.url;
-		this.inputParam = options.inputParam;
-		this.otherParams = options.otherParams;
-		this.oUl = $("<ul class='drop-list'></ul>");
-		this.index = -1;
-		this.ajaxType = options.ajaxType || "post";
-		this.options = options;
-		
-		this.inputField.on("keyup click",function(e){
-			var e = e || window.event;
-			e.stopPropagation();
-			
-			if ( e.type == "click") {
-				self.renderList(self.getNameList());
-			} else{
-				if ( e.keyCode == 40 && self.oUl.children().length > 0) {
-					self.doMove(1);
-				}else if ( e.keyCode == 38 && self.oUl.children().length > 0){
-					self.doMove(-1);
-				}else if ( e.keyCode == 13 && self.oUl.children().length > 0 && self.oUl.children().hasClass("active")){
-					self.doInput(self.oUl.children().filter("[class='active']").html());
-				}else {
-					self.renderList(self.getNameList());
-				}
-			}
-			
-		});
-		
-		
-		this.oUl.on("click",function(e){
-			var e = e || window.event;
-			e.stopPropagation();
-			if ( e.target.nodeName === "LI") {
-				self.doInput(e.target.innerHTML)
-			}
-		});
-		
-		$(document).on("click",function(){
-			self.oUl.empty();
-		});
-	}
-	
-	DropSearch.prototype.getNameList = function () {
-		var self = this,
-			list = [],
-			param = [];
-		param.push(this.inputParam + "=" + this.inputField.val());
-		if ( this.otherParams) {
-			for (var item in this.otherParams) {
-				param.push( item + "=" + this.otherParams[item]);
-			}
+(function($) {
+
+    var DropSearch = function(options) {
+        var self = this;
+        this.inputField = $(options.selector);
+        this.searchUrl = options.url;
+        this.inputParam = options.inputParam;
+        this.otherParams = options.otherParams;
+        this.debounceTime = options.debounceTime;
+        this.oUl = $("<ul class='drop-list'></ul>");
+        this.index = -1;
+        this.ajaxType = options.ajaxType || "post";
+        this.options = options;
+
+        var lock = false;
+
+        this.inputField.on('input focus', debounce(function(e) {
+            if (lock || !$(this).val()) return;
+            self.renderList(self.getNameList());
+        }, self.debounceTime)).on('compositionstart', function(e) {
+            lock = true;
+        }).on('compositionend', function(e) {
+            lock = false;
+            $(this).trigger('input');
+        })
+
+        $(document).on('keyup', function(e) {
+            var keyCode = +e.keyCode;
+
+            if (e.keyCode == 40 && self.oUl.children().length > 0) {
+                self.doMove(1);
+            } else if (e.keyCode == 38 && self.oUl.children().length > 0) {
+                self.doMove(-1);
+            } else if (e.keyCode == 13 && self.oUl.children().length > 0 && self.oUl.children().hasClass("active")) {
+                self.doInput(self.oUl.children().filter("[class='active']").html());
+            }
+        });
+
+
+        this.oUl.on("click", function(e) {
+            var e = e || window.event;
+            e.stopPropagation();
+            if (e.target.nodeName === "LI") {
+                self.doInput(e.target.innerHTML)
+            }
+        });
+
+        $(document).on("click", function() {
+            self.oUl.empty();
+        });
+    }
+
+    DropSearch.prototype.getNameList = function() {
+        var self = this,
+            list = [],
+            param = [];
+        param.push(this.inputParam + "=" + this.inputField.val());
+        if (this.otherParams) {
+            for (var item in this.otherParams) {
+                param.push(item + "=" + this.otherParams[item]);
+            }
+        }
+        $.ajax({
+            type: self.ajaxType,
+            url: self.searchUrl,
+            data: param.join("&"),
+            async: false,
+            success: function(data) {
+                self.options.ajaxHandler && self.options.ajaxHandler(data, list);
+            }
+        });
+        return list;
+    }
+
+    DropSearch.prototype.renderList = function(list) {
+        if (!list || !list.length) { 
+			this.oUl.empty();
+			return;
 		}
-		$.ajax({
-			type: self.ajaxType,
-			url: self.searchUrl,
-			data: param.join("&"),
-			async: false,
-			success: function (data){
-				$.each(data.books, function(i,item) {
-					list.push(item.publisher);
-				});
-			}
-		});
-		return list;
-	}
- 	
- 	DropSearch.prototype.renderList = function (list) {
- 		this.index = -1;
- 		var self = this,
- 			html = [];
- 		self.oUl.empty();
- 		$.each(list, function(i) {
- 			html += "<li>"+list[i]+"</li>";
- 		});
- 		self.oUl.append(html);
- 		self.inputField.parent().append(self.oUl).css("position","relative");
- 		self.oUl.css({
- 			top: self.inputField.outerHeight()+2,
- 			left: self.inputField.get(0).offsetLeft,
- 			width: self.inputField.outerWidth()
- 		});
- 	}
- 	
- 	DropSearch.prototype.doMove = function (count) {
- 		var aLi = this.oUl.children();
- 		this.index = this.index + count;
- 		if ( this.index > aLi.length - 1) {
- 			this.index = 0;
- 		} else if ( this.index < 0){
- 			this.index = aLi.length -1;
- 		}
- 		aLi.removeClass("active").eq(this.index).addClass("active");
- 	}
- 	
- 	DropSearch.prototype.doInput = function (data){
- 		this.inputField.val(data);
-		this.oUl.empty();
-		if ( this.options.clickHandler) {
-			this.options.clickHandler(data);
-		}
- 	}
- 	
-	DropSearch.init = function(options){
-		new this(options);
-	};
-	
-	window["DropSearch"] = DropSearch;
+        this.index = -1;
+        var self = this,
+            html = [];
+        self.oUl.empty();
+        $.each(list, function(i) {
+            html += "<li>" + list[i] + "</li>";
+        });
+        self.oUl.append(html);
+        self.inputField.parent().append(self.oUl).css("position", "relative");
+        self.oUl.css({
+            top: self.inputField.outerHeight() + 2,
+            left: self.inputField.get(0).offsetLeft,
+            width: self.inputField.outerWidth()
+        });
+    }
+
+    DropSearch.prototype.doMove = function(count) {
+        var aLi = this.oUl.children();
+        this.index = this.index + count;
+        if (this.index > aLi.length - 1) {
+            this.index = 0;
+        } else if (this.index < 0) {
+            this.index = aLi.length - 1;
+        }
+        aLi.removeClass("active").eq(this.index).addClass("active");
+    }
+
+    DropSearch.prototype.doInput = function(data) {
+        this.inputField.val(data);
+        this.oUl.empty();
+        if (this.options.clickHandler) {
+            this.options.clickHandler(data);
+        }
+    }
+
+    DropSearch.init = function(options) {
+        new this(options);
+    };
+
+    function debounce(fn, interval) {
+        var timeout = null;
+        interval = interval || 300;
+        return function() {
+            var self = this;
+            clearTimeout(timeout);
+            timeout = setTimeout(function() {
+                fn.apply(self, arguments);
+            }, interval);
+        };
+    }
+
+    window["DropSearch"] = DropSearch;
 })(jQuery);
